c4073d430a9a883fdca442878bc6cb74a3ec8b5e
kukka <lucas.h.fernandes.reis@gmail.com>
2023-09-05 20:40:49 -0300

    fix: add social midias redirect

--- src/components/Projects/ProjectsItem.tsx
+++ src/components/Projects/ProjectsItem.tsx
@@ -12,6 +12,7 @@ interface IProjectProps {
 }
 
 const ProjectsItem = ({ id, title, description, onClick }: IProjectProps) => {
+<<<<<<< HEAD
   const { Paragraph } = Typography;
   const defaultShadow = "0px 4px 8px 0px rgba(0, 0, 0, 0.1)";
   const hoverShadow = "0px 4px 12px 5px rgba(0, 0, 0, 0.12)";
@@ -45,5 +46,40 @@ const ProjectsItem = ({ id, title, description, onClick }: IProjectProps) => {
       </Paragraph>
     </Card>
   );
+=======
+	const { Paragraph } = Typography;
+	const defaultShadow = "0px 4px 8px 0px rgba(0, 0, 0, 0.1)";
+	const hoverShadow = "0px 4px 12px 5px rgba(0, 0, 0, 0.12)";
+	const [shadow, setShadow] = useState(defaultShadow);
+	const onMouseOver = () => setShadow(hoverShadow);
+	const onMouseOut = () => setShadow(defaultShadow);
+	const navigate = useNavigate();
+
+	return (
+		<Card
+			onMouseOver={onMouseOver}
+			onMouseOut={onMouseOut}
+			onClick={onClick}
+			actions={[
+				<Tooltip title="Settings" key="setting" placement="bottom">
+					<SettingOutlined
+						onClick={e => {
+							e.stopPropagation();
+							navigate(routesManager.getSourcesRoute(id));
+						}}
+					/>
+				</Tooltip>,
+			]}
+			type="inner"
+			title={title}
+			hoverable
+			style={{ boxShadow: shadow, minWidth: 180 }}
+		>
+			<Paragraph ellipsis={{ rows: 2 }} style={{ height: "2.5rem" }}>
+				{description}
+			</Paragraph>
+		</Card>
+	);
+>>>>>>> 08835db (fix: add social midias redirect)
 };
 export { ProjectsItem };
