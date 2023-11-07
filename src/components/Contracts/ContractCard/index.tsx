161ae8bd3c5762b4f296b200fb98d185de9e1117
kukka <lucas.h.fernandes.reis@gmail.com>
2023-11-07 17:12:35 -0300

    fix: close select field after select a option

--- src/components/Contracts/ContractCard/index.tsx
+++ src/components/Contracts/ContractCard/index.tsx
@@ -1,6 +1,6 @@
-import { Modal, Space, Tooltip, Typography } from "antd";
+import { Space, Tooltip, Typography } from "antd";
 import { CardStyled, TitleContainer } from "./styled";
-import { IContract, contractsServices } from "../../../services";
+import { IContract } from "../../../services";
 import { useNavigate } from "react-router-dom";
 import { routesManager } from "../../../routes/routesManager";
 import {
@@ -10,53 +10,21 @@ import {
   QuestionCircleOutlined,
 } from "@ant-design/icons";
 import { useTranslation } from "react-i18next";
-import { useAppDispatch } from "../../../redux/hooks";
-import { actionDisplayNotification } from "../../../redux/notificationSlice";
 
 type ContractCardProps = {
   contract: IContract;
   setContractToEdit: React.Dispatch<React.SetStateAction<IContract | undefined>>;
   fetchContracts: () => Promise<void>;
+  handleDeleteContract: (contract: IContract) => void;
 };
 
 const ContractCard: React.FC<ContractCardProps> = ({
   contract,
   setContractToEdit,
-  fetchContracts,
+  handleDeleteContract,
 }) => {
   const navigate = useNavigate();
   const { t } = useTranslation();
-  const dispatch = useAppDispatch();
-
-  const warning = () => {
-    Modal.confirm({
-      title: t("global.confirm"),
-      content: t("pages.contracts.components.warning.areYouSureToDeleteContract", {
-        contract_name: contract.title,
-      }),
-      onOk: async () => {
-        try {
-          await contractsServices.deleteContract(contract.id);
-          fetchContracts();
-          dispatch(
-            actionDisplayNotification({
-              severity: "success",
-              messages: [t("global.successDeletedMessage")],
-            }),
-          );
-        } catch {
-          dispatch(
-            actionDisplayNotification({
-              severity: "warning",
-              messages: [t("global.failureDeleteMessage")],
-            }),
-          );
-        }
-      },
-      cancelText: t("global.cancel"),
-      okText: t("global.delete"),
-    });
-  };
 
   return (
     <CardStyled
@@ -83,7 +51,7 @@ const ContractCard: React.FC<ContractCardProps> = ({
           <DeleteOutlined
             onClick={(e) => {
               e.stopPropagation();
-              warning();
+              handleDeleteContract(contract);
             }}
           />
         </Tooltip>,
