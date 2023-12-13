5626994acc2f93d8615d702e96a31f3f4620506f
Lorenzo C <lorenzo_cesconeto@hotmail.com>
2023-12-12 21:02:29 -0300

    Show login error upon fail to login with Google

--- src/components/Login/GoogleLogin/index.tsx
+++ src/components/Login/GoogleLogin/index.tsx
@@ -3,13 +3,34 @@ import { useAppDispatch } from "../../../redux/hooks";
 import { actionDisplayNotification } from "../../../redux/notificationSlice";
 import { authService } from "../../../services";
 import { actionLoginWithGoogle } from "../../../redux/authenticationSlice";
-
+import axios, { AxiosError } from "axios";
 const GoogleLogin = () => {
   const dispatch = useAppDispatch();
 
   const handleLoginSuccess = async (response: CredentialResponse) => {
-    const data = await authService.signUpOrInWithGoogle(response);
-    dispatch(actionLoginWithGoogle(data));
+    try {
+      const data = await authService.signUpOrInWithGoogle(response);
+      dispatch(actionLoginWithGoogle(data));
+    } catch (err) {
+      const errorMessages = ["Failed to authenticate with Google"];
+      if (axios.isAxiosError(err)) {
+        const axiosError = err as AxiosError;
+        if (
+          axiosError.response &&
+          axiosError.response.data &&
+          (axiosError.response.data as { detail?: string }).detail
+        ) {
+          const errorMsg = (axiosError.response.data as { detail: string }).detail;
+          errorMessages.push(errorMsg);
+        }
+      }
+      dispatch(
+        actionDisplayNotification({
+          messages: errorMessages,
+          severity: "error",
+        }),
+      );
+    }
   };
 
   const onError = () => {
